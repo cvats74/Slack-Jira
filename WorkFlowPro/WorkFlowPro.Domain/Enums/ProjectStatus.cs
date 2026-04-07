4e4438dc32ba68c82843ea407eccd14983ced111
Chirag Vats <chiragvats134@gmail.com>
2026-04-07 20:10:15 +0530

    commit all

---
+++ WorkFlowPro/WorkFlowPro.Domain/Enums/ProjectStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkFlowPro.Domain.Enums
+{
+    public enum ProjectStatus
+    {
+        Planning = 1,
+        Active = 2,
+        OnHold = 3,
+        Completed = 4,
+        Cancelled =5
+    }
+}
---
+++ WorkFlowPro/WorkFlowPro.Domain/Enums/TaskStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkFlowPro.Domain.Enums
+{
+    public enum TaskStatus
+    {
+        ToDo = 1,
+        InProgress = 2,
+        InReview = 3,
+        Blocked = 4,
+        Done = 5,
+        Cancelled = 6
+    }
+}
