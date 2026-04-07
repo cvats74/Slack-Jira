4942fbc3cd091e979904738d9064d339a0f90bbd
Chirag Vats <chiragvats134@gmail.com>
2026-04-07 20:12:59 +0530

    commit all

--- WorkFlowPro/WorkFlowPro.API/obj/Debug/net8.0/WorkFlowPro.API.AssemblyInfo.cs
+++ WorkFlowPro/WorkFlowPro.API/obj/Debug/net8.0/WorkFlowPro.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkFlowPro.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de4e891c97d8ee87bb8f4d28708a6980aedd6989")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkFlowPro.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkFlowPro.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
