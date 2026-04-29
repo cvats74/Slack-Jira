973fc0b40f3ee4f0867bbda12d841644843b19da
Chirag Vats <chiragvats134@gmail.com>
2026-04-29 20:18:15 +0530

    workitem commit

---
+++ WorkFlowPro/WorkFlowPro.Infrastructure/Repositories/WorkItemRepository.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkFlowPro.Application.Common.Interfaces;
+using WorkFlowPro.Domain.Entities;
+using WorkFlowPro.Infrastructure.Data;
+
+namespace WorkFlowPro.Infrastructure.Repositories
+{
+    public class WorkItemRepository : IWorkItemRepository
+    {
+
+        private readonly AppDbContext _context;
+
+        public async Task<bool> BelongsToProjectAsync(Guid workItemId, Guid projectId)
+        {
+            return await _context.WorkItems.AnyAsync(w => w.Id  == workItemId && w.ProjectId == projectId); 
+        }
+
+        public async Task<WorkItem> CreateAsync(WorkItem workItem)
+        {
+            await _context.WorkItems.AddAsync(workItem);
+            return workItem;
+        }
+
+        public async Task<IEnumerable<WorkItem>> GetByAssigneeAsync(Guid userId)
+        {
+            return await _context.WorkItems
+                .Include(w => w.Project)
+                .Where(w => w.AssigneeId == userId)
+                .OrderByDescending(w => w.CreatedAt).ToListAsync();
+        }
+
+        public async Task<WorkItem?> GetByIdAsync(Guid id)
+        {
+            return await _context.WorkItems.FirstOrDefaultAsync(w => w.Id == id);
+        }
+
+        public async Task<WorkItem?> GetByIdWithDetailsAsync(Guid id)
+        {
+            return await _context.WorkItems.Include(w => w.Project)
+                .Include(w => w.Assignee)
+                .Include(w => w.Reporter)
+                .Include(w => w.SubTasks)
+                .Include(w => w.Comments)
+                .FirstOrDefaultAsync( w  => w.Id == id);
+        }
+
+        public async Task<IEnumerable<WorkItem>> GetByProjectAndStatusAsync(Guid projectId, Domain.Enums.TaskStatus status)
+        {
+            return await _context.WorkItems
+                .Include(w => w.Assignee)
+                .Include(w => w.SubTasks)
+                .Where(w =>
+                    w.ProjectId == projectId &&
+                    w.Status == status &&
+                    w.ParentTaskId == null)
+                .OrderBy(w => w.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<WorkItem>> GetByProjectAsync(Guid projectId)
+        {
+            return await _context.WorkItems
+                .Include(w => w.Assignee)
+                .Include(w => w.SubTasks)
+                .Where(w =>
+                    w.ProjectId == projectId &&
+                    w.ParentTaskId == null)
+                .OrderBy(w => w.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<WorkItem>> GetSubTaskAsync(Guid parentTaskId)
+        {
+            return await _context.WorkItems
+                .Include(w => w.Assignee)
+                .Where(w => w.ParentTaskId == parentTaskId)
+                .ToListAsync();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public Task<WorkItem> UpdateAsync(WorkItem workItem)
+        {
+            _context.Update(workItem);
+            return Task.FromResult(workItem);
+        }
+    }
+}
---
+++ WorkFlowPro/WorkFlowPro.Infrastructure/Services/WorkItemService.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkFlowPro.Application.Common.Interfaces;
+using WorkFlowPro.Application.Features.WorkItems.DTOs;
+using WorkFlowPro.Domain.Entities;
+
+namespace WorkFlowPro.Infrastructure.Services
+{
+    public class WorkItemService : IWorkItemService
+    {
+        private readonly IProjectRepository _projectRepository;
+        private readonly IWorkItemRepository _workItemRepository;
+        private readonly IUserRepository _userRepository;
+
+        public WorkItemService(IProjectRepository projectRepository, IWorkItemRepository workItemRepository, IUserRepository userRepository)
+        {
+            _projectRepository = projectRepository;
+            _workItemRepository = workItemRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task<WorkItemResponseDto> CreateAsync(Guid projectId, CreateWorkItemDto dto, Guid currentUserId)
+        {
+            var project = await _projectRepository.GetByIdAsync(projectId);
+
+            if (project == null) {
+
+                throw new KeyNotFoundException("Project not found");
+            }
+
+            var isMember = await _projectRepository.IsUserMemberAsync(projectId, currentUserId);
+
+            if(!isMember && project.OwnerId != currentUserId)
+            {
+                throw new UnauthorizedAccessException("Only project members can create tasks");
+            }
+
+            if (dto.AssigneeId.HasValue)
+            {
+                var assigneeIsMember = await _projectRepository.IsUserMemberAsync(projectId, currentUserId);
+                if (!assigneeIsMember)
+                {
+                    throw new InvalidOperationException("Assignee must be project member");
+
+                }
+            }
+            var workItem = new WorkItem
+            {
+                Title = dto.Title,
+                Description = dto.Description,
+                Priority = dto.Priority,
+                DueDate = dto.DueDate,
+                AssigneeId = dto.AssigneeId,
+                ParentTaskId = dto.ParentTaskId,
+                EstimatedHours = dto.EstimatedHours,
+                ProjectId = projectId,
+                ReporterId = currentUserId,
+                Status = Domain.Enums.TaskStatus.ToDo,
+                TenantId = project.TenantId,
+                CreatedBy = currentUserId
+            };
+
+            await _workItemRepository.CreateAsync(workItem);
+            await _workItemRepository.SaveChangesAsync();
+
+            var created = await _workItemRepository.GetByIdWithDetailsAsync(workItem.Id);
+            return MapToResponseDto(created!);
+        }
+
+       
+
+        public async Task DeleteAsync(Guid workItemId, Guid currentUserId)
+        {
+            var workItem = await _workItemRepository.GetByIdWithDetailsAsync(currentUserId);
+            if (workItem != null)
+            {
+                throw new KeyNotFoundException("Task not found");
+            }
+
+            var isMember = await _projectRepository.IsUserMemberAsync(workItem.ProjectId, currentUserId);
+            var isOwner = workItem.Project?.OwnerId == currentUserId;
+
+            if (!isMember && !isOwner)
+            {
+                throw new UnauthorizedAccessException("Access Denied");
+            }
+        }
+
+        public Task<WorkItemResponseDto> GetByIdAsync(Guid workItemId, Guid currentUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<WorkItemSummaryDto>> GetByProjectAsync(Guid projectId, Guid currentUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WorkItemResponseDto> UpdateAsync(Guid workItemId, UpdateWorkItemDto dto, Guid currentUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WorkItemResponseDto> UpdateStatusAsync(Guid workItemId, UpdateWorkItemStatusDto dto, Guid currentUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static WorkItemResponseDto MapToResponseDto(WorkItem w)
+        {
+            return new WorkItemResponseDto
+            {
+                Id = w.Id,
+                Title = w.Title,
+                Description = w.Description,
+                Status = w.Status.ToString(),
+                Priority = w.Priority.ToString(),
+                DueDate = w.DueDate,
+                CreatedAt = w.CreatedAt,
+                EstimatedHours = w.EstimatedHours,
+                ActualHours = w.ActualHours,
+                ProjectId = w.ProjectId,
+                ProjectName = w.Project?.Name
+                    ?? string.Empty,
+                ReporterId = w.ReporterId,
+                ReporterName = w.Reporter?.FullName
+                    ?? string.Empty,
+                AssigneeId = w.AssigneeId,
+                AssigneeName = w.Assignee?.FullName,
+                SubTasks = w.SubTasks?
+                    .Select(s => MapToSummaryDto(s))
+                    .ToList() ?? new(),
+                CommentCount = w.Comments?.Count ?? 0
+            };
+        }
+
+        private static WorkItemSummaryDto MapToSummaryDto(
+            WorkItem w)
+        {
+            return new WorkItemSummaryDto
+            {
+                Id = w.Id,
+                Title = w.Title,
+                Status = w.Status.ToString(),
+                Priority = w.Priority.ToString(),
+                DueDate = w.DueDate,
+                AssigneeId = w.AssigneeId,
+                AssigneeName = w.Assignee?.FullName,
+                SubTaskCount = w.SubTasks?.Count ?? 0,
+                CommentCount = w.Comments?.Count ?? 0
+            };
+        }
+    }
+}
